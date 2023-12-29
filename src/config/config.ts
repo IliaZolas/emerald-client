f96f5298054fd63cfebbc86bd457ec271cf70ce0
Ilia Zolas <ilia@thedmcsa.co.za>
2023-12-29 18:24:12 +0100

    feature/EME-19 - dotenv not easily possible and other confirgurations not working, reverted to previous route

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 const production = {
-        url: process.env["PROD_SERVER_URL"] 
+        url: 'https://emerald-server-production.up.railway.app'    
     };
 const development = {
         url: 'http://localhost:4000'
