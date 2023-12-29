1288ae01240f22c937303aa77002863e4f6a0d71
Ilia Zolas <ilia@thedmcsa.co.za>
2023-12-29 11:00:26 +0100

    feature/EME-5 & EME-6 - used angular material for form ui, added fetch request, render response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  purchasePrice: number = 0; 
+  monthlyRent: number = 0;
+  annualRentalFee: number = 0;
+
+  roiY1: number = 0;
+  roiY2: number = 0;
+  roiY3: number = 0;
+  overallROI: number = 0;
+
+  constructor() {}
+
+  handleSubmit() {
+    this.makeFetchRequest();
+    console.log('Form submitted!', this.purchasePrice, this.monthlyRent, this.annualRentalFee);
+  }
+
+  private makeFetchRequest() {
+    const data = {
+      purchasePrice: this.purchasePrice,
+      monthlyRent: this.monthlyRent,
+      annualRentalFee: this.annualRentalFee
+    };
+
+    fetch('http://localhost:4000/calculate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log('Calculation result:', result);
+        this.roiY1 = result.roiY1;
+        this.roiY2 = result.roiY2;
+        this.roiY3 = result.roiY3;
+        this.overallROI = result.overallROI;
+      })
+      .catch(error => {
+        console.error('Error during fetch:', error);
+      });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +20,11 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
